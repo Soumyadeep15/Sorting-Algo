f55d0d386499f07f8e2f3574ee2506d0e6962842
Soumyadeep Saha Roy <108778550+Soumyadeep15@users.noreply.github.com>
2023-07-06 10:41:34 +0530

    Update AllinOneSorting.java

--- Sorting Algorithm/AllinOneSorting.java
+++ Sorting Algorithm/AllinOneSorting.java
@@ -11,7 +11,7 @@ public class AllinOneSorting {
         }
         System.out.println();
     }
-    //BUbbleSort
+    //BubbleSort
     public static void bubbleSort(int[] a){
         for(int i=0;i<a.length-1;i++){
             for(int j=0;j<a.length-i-1;j++){
